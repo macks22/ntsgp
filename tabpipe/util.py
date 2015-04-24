f380527428143ccfcfcc8ccdf7386867d27d9c82
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-04-24 17:41:12 -0400

    moved util functions into own module.

---
+++ tabpipe/util.py
@@ -0,0 +1,51 @@
+import luigi
+import luigi.worker
+
+
+def schedule_task(task, verbose=False):
+    if verbose:
+        luigi.interface.setup_interface_logging()
+    sch = luigi.scheduler.CentralPlannerScheduler()
+    w = luigi.worker.Worker(scheduler=sch)
+    w.add(task)
+    w.run()
+
+
+def parse_colnames(thing):
+    """Can be str, csv string, or tuple of str."""
+    if hasattr(thing, '__len__'):
+        return thing
+    elif ',' in thing:
+        return thing.split(',')
+    elif isinstance(thing, basestring):
+        return thing
+    else:
+        raise ValueError('invalid primary id: {}'.format(thing))
+
+
+def abbrev_names(names):
+    """Abbreviate colnames using as few letters as possible and combine them
+    using title casing, with 0 spaces in between. For terms with gooseneck
+    naming, the first letter of each term up to the last is used (in caps), and
+    the last first few letters are taken from the last name. So for instance,
+    colnames=('nation_zip', 'nation_code', 'name', 'cum_gpa', 'term_gpa') would
+    give: ZiCoNaCuTodNaCGpaTGpa. We use 2 letters minimum from each colname,
+    unless it only has one.
+    """
+    if not names:
+        return ''
+
+    # first break up the gooseneck names.
+    abbrevs = []
+    terms = [name.split('_') for name in names]
+    for words in terms:
+        if len(words) == 1:
+            abbrev = words[0][:2]
+            abbrev = ''.join([abbrev[0].upper(), abbrev[1:3]])
+        else:
+            last_term = words[-1]
+            prefix = ''.join([word[0] for word in words[:-1]]).upper()
+            abbrev = ''.join([prefix, last_term[0].upper(), last_term[1:3]])
+        abbrevs.append(abbrev)
+
+    return ''.join(abbrevs)
