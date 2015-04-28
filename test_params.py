9edfb9991bbaf2c11dd3aff7d23e5b46da22e246
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-04-27 22:38:46 -0400

    changed default back to no bias terms

--- test_params.py
+++ test_params.py
@@ -34,7 +34,7 @@ def compose_libfm_args(train, test, iter=20, std=0.2, dim=8, bias=False,
         '-train', train,
         '-test', test,
         '-iter', str(iter),
-        '-dim', '1,%d,%d' % (bias, dim),
+        '-dim', '%d,%d,%d' % (bias, bias, dim),
         '-init_stdev', str(std)
     ]
     if outfile:
