65a1f94e926f68eb24264dfb354c40ed23b6f9b7
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-06-11 18:49:23 -0400

    libfm wrapper refactor to improve modularity

---
+++ methods/old_libfm.py
@@ -0,0 +1,351 @@
+import os
+import subprocess as sub
+import argparse
+import logging
+import datetime
+import time
+
+import numpy as np
+from sklearn import preprocessing
+
+from writer import write_libfm
+from scaffold import *
+
+
+LIBFM = '/home/msweene2/ers-data/libfm-1.42.src/bin/libFM'
+
+
+class LibFMFailed(Exception):
+    """LibFM returned a non-zero exit code."""
+    pass
+
+
+def compose_libfm_args(train, test, iter=20, std=0.2, dim=8, bias=False,
+                       outfile='', task='r', bin=LIBFM, fbias=False,
+                       gbias=False):
+    """Put together libFM args in a list suitable for use with Popen.
+
+    :param str train:   Path for training data file, in libFM format.
+    :param str test:    Path for test data file, in libFM format.
+    :param int iter:    Number of iterations to run learning algorithm for.
+    :param int dim:     Dimensionality of low-rank approximation to use.
+    :param bool bias:   Use global and per-feature bias terms.
+    :param str outfile: Output predictions to a file with this name.
+    :return:            List of args to use for running libFM.
+
+    """
+    if bias:
+        fbias, gbias = 1, 1
+    else:
+        fbias = int(fbias)
+        gbias = int(gbias)
+
+    bias = int(bias)
+    args = [
+        bin,
+        '-task', task,
+        '-train', train,
+        '-test', test,
+        '-iter', str(iter),
+        '-dim', '%d,%d,%d' % (gbias, fbias, dim),
+        '-init_stdev', str(std)
+    ]
+    if outfile:
+        args.append('-out')
+        args.append(outfile)
+    return args
+
+
+def run_libfm(train, test, iter=20, std=0.2, dim=8, bias=False,
+              outfile='', task='r', fbias=False, gbias=False):
+    """Run libFM and return final train/test results.
+
+    :return: Final error for (train, test) sets.
+    """
+    kwargs = {k: v for k, v in locals().items() if not k in ['train', 'test']}
+    args = compose_libfm_args(train, test, **kwargs)
+    cmd = ' '.join(args)
+    logging.debug(cmd)
+
+    proc = sub.Popen(cmd, shell=True, stdout=sub.PIPE)
+    retcode = proc.wait()
+    if retcode:
+        raise LibFMFailed("libFM failed to execute.\n%s" % cmd)
+
+    output = proc.communicate()[0]
+    lines = output.split('\n')
+    rows = [row.split('\t')[1:] for row in lines[-iter:] if row]
+    train_err = '%.6f' % float(rows[-1][0].split('=')[1])
+    test_err = '%.6f' % float(rows[-1][1].split('=')[1])
+    return [train_err, test_err]
+
+
+def libfm_predict(train, test, outfile, iter=20, std=0.2, dim=8, bias=False,
+                  task='r', fbias=False, gbias=False):
+    """Run libFM, output predictions to file, read file and return predictions
+    as an array of floats.
+
+    :rtype: numpy.ndarray of floats.
+    :return: The predictions for the test examples.
+    """
+    kwargs = {k: v for k, v in locals().items()
+              if not k in ['train', 'test']}
+    kwargs['outfile'] = outfile
+    args = compose_libfm_args(train, test, **kwargs)
+    cmd = ' '.join(args)
+    logging.debug(cmd)
+
+    proc = sub.Popen(cmd, shell=True, stdout=sub.PIPE)
+    retcode = proc.wait()
+    if retcode:
+        raise LibFMFailed("libFM failed to execute.\n%s" % cmd)
+
+    # read output and return predictions
+    with open(outfile) as f:
+        return np.array([float(num) for num in f if num])
+
+
+class FM(object):
+    """Factorization machine (wraps libFM functionality using subprocess."""
+
+    def __init__(self, train, test, outdir='tmp', iter=20, std=0.2, dim=8,
+                 bias=False, fbias=False, gbias=False, task='r', userid='sid',
+                 itemid='cid', target='grdpts', cvals=None, rvals=None,
+                 pcgrades=False):
+        """
+        :param DataFrame train: Training data.
+        :param DataFrame test: Test data.
+        :param str outfile: Path of file to write predictions to.
+        :param int iter: Number of iterations to learn for.
+        :param float std: Standard deviation of Gaussian noise for
+            model initialization.
+        :param int dim: Dimensionaltiy of model.
+        :param bool bias: Use global and per-feature bias if True.
+        :param str task: r=regression, c=classification.
+
+        """
+        tmpdir = '.tmp'
+        try:
+            os.mkdir(tmpdir)
+        except OSError:
+            pass
+
+        self.outdir = os.path.join(tmpdir, outdir)
+        try:
+            os.mkdir(self.outdir)
+        except OSError:
+            pass
+
+        self.outfile = os.path.join(self.outdir, 'predict.csv')
+        self.ftrain = os.path.join(self.outdir, 'train.csv')
+        self.ftest = os.path.join(self.outdir, 'test.csv')
+
+        # Write data in libFM format to temporary files.
+        with open(self.ftrain, 'w') as ftrain, open(self.ftest, 'w') as ftest:
+            write_libfm(ftrain, ftest, train, test, target, userid,
+                        itemid, cvals, rvals, pcgrades)
+
+        self.iter = iter
+        self.std = std
+        self.dim = dim
+        self.bias = bias
+        self.gbias = gbias
+        self.fbias = fbias
+        self.task = task
+
+    def run(self):
+        predictions = libfm_predict(
+            self.ftrain, self.ftest, self.outfile, self.iter, self.std,
+            self.dim, self.bias, self.task, self.fbias, self.gbias)
+        return np.round(predictions) if self.task == 'c' else predictions
+
+
+def fm_mcmc(method='mcmc', task='r', iter=100, std=0.1, dim=8,
+            fbias=True, gbias=True, lrate=0.1, r0=0.0, r1=0.0, r2=0.0,
+            outdir='tmp', logfile=''):
+    return fm.run()
+
+
+def libfm_model(train, test, *args, **kwargs):
+    train = train.copy()
+    to_predict = test.copy()
+    logging.info('predicting %d values using libFM' % len(to_predict))
+    to_predict['grdpts'] = fm_mcmc(train, test, *args, **kwargs)
+    return to_predict
+
+
+def scale(df, colname):
+    df[colname] = (df[colname] - df[colname].mean()) / df[colname].std(ddof=0)
+
+
+def libfm_model2(train, test, *args, **kwargs):
+    """Scale data first."""
+    target = kwargs.get('target', 'grdpts')
+    task = kwargs.get('task', 'r')
+    task_name = 'regression' if task == 'r' else 'classification'
+
+    train = train.copy()
+    to_predict = test.copy()
+
+    # Scale real-valued attributes.
+    # Note: we must scale both train/test at the same time in order to avoid
+    # distorting the importance of the attributes in one or the other.
+    for colname in kwargs.get('rvals', []):
+        vals = pd.concat((train[colname], test[colname]))
+        mean = vals.mean()
+        std = vals.std(ddof=0)
+        train[colname] = (train[colname] - mean) / std
+        test[colname] = (test[colname] - mean) / std
+
+    logging.info('predicting %d %s values using libFM %s' % (
+        len(to_predict), target, task_name))
+    to_predict[target] = fm_mcmc(train, test, *args, **kwargs)
+    return to_predict
+
+
+def test_dim(start, end, *args, **kwargs):
+    """Run libFM regression once for each dimension value in range(start, end+1).
+    See `run_libfm` for *args and **kwargs.
+
+    :param int start: The first dimension in the range.
+    :param int end: The last dimension in the range (inclusive).
+    :return: List of (dim, train_err, test_err) for all dimensions tested.
+    """
+    results = []
+    for dim in range(start, end+1):
+        out = run_libfm(*args, dim=dim, **kwargs)
+        out.insert(0, str(dim))
+        logging.info('\t'.join(out))
+        results.append(out)
+
+    top = list(sorted(results, key=lambda l: l[2]))
+    top5 = [t[0] for t in top[:5]]
+    logging.info('best results with dim=%s' % ','.join(map(str, top5)))
+    return results
+
+
+def gen_ts():
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+    return st
+
+
+CVALS = ['itenure', 'irank', 'iclass', 'iid',
+         'major', 'hs', 'sex', 'zip', 'srace', 'cohort',
+         'cdisc']
+RVALS = ['age', 'hsgpa', 'sat', 'lterm_gpa', 'lterm_cum_gpa',
+         'chrs', 'clevel', 'total_chrs', 'num_enrolled', 'total_enrolled',
+         'lterm_cgpa', 'lterm_cum_cgpa', 'term_chrs']
+
+def make_parser():
+    parser = base_parser('try out various libFM params to fine-tune')
+    parser.add_argument(
+        '-b', '--bias', action='store_true', default=False,
+        help='bias terms will be used if this flag is given')
+    parser.add_argument(
+        '-fb', '--fbias', action='store_true', default=False)
+    parser.add_argument(
+        '-gb', '--gbias', action='store_true', default=False)
+    parser.add_argument(
+        '-i', '--iter', type=int, default=200,
+        help='number of iterations to run learning algorithm for')
+    parser.add_argument(
+        '-d', '--dimension', type=int, default=5,
+        help='dimensionality of low-rank approximation')
+    # higher values can speed up learning, but too high can slow it
+    parser.add_argument(
+        '-s', '--init-stdev', type=float, default=0.2,
+        help='initial standard deviation for initial Gaussian dist')
+    parser.add_argument(
+        '-p', '--pcgrades', action='store_true', default=False)
+    parser.add_argument(
+        '-t', '--task',
+        action='store', choices=('r', 'c'), default='r')
+    parser.add_argument(
+        '-o', '--outdir',
+        action='store', default=None)
+
+    # Add all possible features.
+    for featname in CVALS + RVALS:
+        name = '--%s' % featname
+        parser.add_argument(
+            name, action='store_true', default=False)
+
+    return parser
+
+
+if __name__ == "__main__":
+    args = setup(make_parser)
+    outdir = args.outdir if args.outdir else gen_ts()
+
+    data = pd.read_csv(args.data_file).sort(['sid', 'termnum'])
+    cvals = [cval for cval in CVALS if getattr(args, cval)]
+    rvals = [rval for rval in RVALS if getattr(args, rval)]
+
+    target = 'grdpts' if args.task == 'r' else 'alevel'
+    if not target in data.columns:
+        print "cannot perform %s task without target attribute: %s" % (
+            'regression' if args.task == 'r' else 'classification', target)
+        sys.exit()
+
+    def eval_fm(train, test):
+        return libfm_model2(
+            train, test, outdir=outdir, iter=args.iter, std=args.init_stdev,
+            dim=args.dimension, bias=args.bias, fbias=args.fbias,
+            gbias=args.gbias, task=args.task, target=target,
+            cvals=cvals, rvals=rvals, pcgrades=args.pcgrades)
+
+    if args.task == 'r':
+        quiet_delete(data, 'alevel')
+
+        results = method_error(data, eval_fm, False)
+        evaluation = eval_results(
+            results, by='sterm' if args.plot == 'sterm' else 'termnum')
+        print evaluation
+
+        if args.plot == 'pred':
+            g1, g2 = plot_predictions(results)
+        if args.plot == 'term':
+            ax1, ax2 = plot_error_by('termnum', results)
+        elif args.plot == 'sterm':
+            ax1, ax2 = plot_error_by('sterm', results)
+
+    else:
+        quiet_delete(data, 'grdpts')
+
+        num_labels = len(data.alevel.unique())
+        results = pd.DataFrame()
+        tokeep = ['sid', 'cid', 'alevel', 'major', 'sterm']
+
+        terms = list(sorted(data['termnum'].unique()))
+        for termnum in terms:
+            logging.info("making predictions for termnum %d" % termnum)
+
+            train, test = split_train_test(data, termnum)
+            test = remove_cold_start(train, test)
+            if len(test) == 0:
+                continue
+
+            # How many labels are we actually working with?
+            uniq_labels = np.unique(test[target])
+            nlabels = len(uniq_labels)
+            if nlabels == 1:
+                print 'only one label for term %d' % termnum
+                continue
+            elif nlabels == 2:
+                predicted = eval_fm(train, test)
+            else:
+                raise NotImplementedError(
+                    "multi-class classification not implemented")
+                sys.exit()
+
+            df = test[tokeep].copy()
+            df['predicted'] = predicted[target]
+            df['test'] = test[target]
+            results = pd.concat((results, df))
+
+            print '\nTERM %d:' % termnum
+            print_eval(df['test'], df['predicted'])
+
+        print '\nTOTALS'
+        print_eval(results['test'], results['predicted'])
---
+++ methods/wlibfm.py
@@ -0,0 +1,202 @@
+"""
+Wrap up LibFM using subprocess calls. This requires that the train/test files be
+written to disk before use and that the results be written to disk after
+predictions. Naturally, this can be quite slow in some cases. It is an
+expeditious solution with an acceptable offline runtime at this time.
+
+NOTE: the current implementation does not support binary data format, meta
+groupings or relations. It also does not support SGDA (since that requires
+extending the writer to write a validation set).
+
+"""
+import os
+import time
+import shutil
+import logging
+import datetime
+import subprocess as sub
+
+import numpy as np
+
+from writer import write_libfm
+
+
+def silent_mkdir(dirname):
+    """Create a directory if it does not exist, else do nothing."""
+    try:
+        os.mkdir(dirname)
+    except OSError:
+        pass
+
+
+class LibFMFailed(Exception):
+    """LibFM returned a non-zero exit code."""
+    pass
+
+
+def gen_ts():
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+    return st
+
+
+LIBFM = '/home/msweene2/ers-data/libfm-1.42.src/bin/libFM'
+class FM(object):
+    """Factorization machine (wraps libFM functionality using subprocess)."""
+
+    def __init__(self, method='mcmc', task='r', iter=100, std=0.1, dim=8,
+                 fbias=True, gbias=True, lrate=0.1, r0=0.0, r1=0.0, r2=0.0,
+                 outdir='', logfile='', verbosity=0, bin=LIBFM):
+        """
+        Args:
+            method (str): Learning method for inference, mcmc by default.
+            task (str): r=regression c=classification, set to r by default.
+            iter (int): Number of iterations to learn for, 100 by default.
+            std (float): Standard deviation of Gaussian noise used for model
+                initialization, 0.1 by default.
+            dim (int): Dimensionality (rank) of the FM. This is the number of
+                latent factors learned, 8 by default.
+            fbias (bool): If True, use per-interaction bias terms, True by
+                default.
+            gbias (bool): If True, use a global bias term (central tendency),
+                True by default.
+            lrate (float): Learning rate for SGD and SGDA, 0.1 by default.
+            r0 (float): Bias regularization term, 0.0 by default.
+            r1 (float): 1-way regularization term, 0.0 by default.
+            r2 (float): 2-way regularization term 0.0 by default.
+            outdir (str): Name of directory to write LibFM-formatted input to,
+                as well as the prediction outputs.
+            logfile (str): Name of the logfile for LibFM logging, which can be
+                made more verbose by increasing the level with the `verbosity`
+                parameter. By default, no logfile is written.
+            verbosity (int): Verbosity level. Higher integers are more verbose.
+                Set to 0 by default.
+
+        """
+        for (k, v) in locals().iteritems():
+            if k != 'self':
+                setattr(self, k, v)
+
+        self.setup(outdir)
+
+    def setup(self, outdir):
+        """Ensure output directory is present in cwd.
+
+        This wrapper requires writing input data frames to disk before calling
+        LibFM and retrieving the output of LibFM from disk in order to return it
+        back in memory. The outdir holds these temporary files, and it must
+        exist before using the wrapper.
+
+        """
+        tmpdir = '.tmp'
+        silent_mkdir(tmpdir)
+        if not outdir:
+            outdir = gen_ts()
+        self.outdir = os.path.join(tmpdir, outdir)
+        silent_mkdir(self.outdir)
+
+    def teardown(self):
+        shutil.rmtree(self.outdir)
+
+    def write(self, train, test, userid='sid', itemid='cid', target='grdpts',
+             cvals=None, rvals=None, previous=False, outfile='predict.csv',
+             train_file='train.csv', test_file='test.csv'):
+        """
+        Args:
+            train (DataFrame): Training dataset; take care that it has columns
+                for `userid`, `itemid`, and `target`, as well as all `cvals`
+                and `rvals`.
+            test (DataFrame): Testing dataset; same precuations as `train`.
+            validation (DataFrame): Validation dataset (only for SGDA).
+            userid (str): Name of the user ID column.
+            itemid (str): Name of the item ID column.
+            target (str): Name of the column with the target (dependent) var.
+            cvals (list): Names of the categorical variables to use as features.
+                This is in addition to `userid` and `itemid` and should not
+                include them (doing so causes undefined behavior).
+            rvals (list): Names of the real-valued variables to use as features.
+                Do not include `target` here.
+            previous (bool): If True, previous item ratings will be used as
+                features.
+            outfile (str): Filename for predictions output file.
+            train_file (str): Filename for temporary LibFM-formatted file for
+                training data.
+            test_file (str): Filename for temporary LibFM-formatted file for
+                testing data.
+
+        """
+        self.outfile = os.path.join(self.outdir, outfile)
+        self.train_file = os.path.join(self.outdir, train_file)
+        self.test_file = os.path.join(self.outdir, test_file)
+
+        # Write data in libFM format to temporary files.
+        with open(self.train_file, 'w') as ftrain,\
+             open(self.test_file, 'w') as ftest:
+            write_libfm(ftrain, ftest, train, test, target, userid,
+                        itemid, cvals, rvals, previous)
+
+    @property
+    def cmd_args(self):
+        args = [
+            self.bin,
+            '-method', self.method,
+            '-task', self.task,
+            '-train', self.train_file,
+            '-test', self.test_file,
+            '-iter', str(self.iter),
+            '-dim', '%d,%d,%d' % (self.gbias, self.fbias, self.dim),
+            '-init_stdev', str(self.std),
+            '-out', self.outfile,
+            '-verbosity', '%d' % self.verbosity
+        ]
+        if self.logfile:
+            args += ['-rlog', self.logfile]
+        if self.method != 'mcmc':
+            args += ['-regular', '%f,%f,%f' % (self.r0, self.r1, self.r2)]
+        if self.method == 'sgd':
+            args += ['-learn_rate', str(self.lrate)]
+        return args
+
+    @property
+    def cmd(self):
+        return ' '.join(self.cmd_args)
+
+    def run(self):
+        """Note that probabilities are returned for classification."""
+        logging.debug(self.cmd)
+
+        proc = sub.Popen(self.cmd, shell=True, stdout=sub.PIPE)
+        retcode = proc.wait()
+        if retcode:
+            raise LibFMFailed("libFM failed to execute.\n%s" % self.cmd)
+
+        # read output and return predictions
+        with open(self.outfile) as f:
+            return np.array([float(num) for num in f if num])
+
+
+if __name__ == "__main__":
+
+    # Read the data.
+    import pandas as pd
+    data = pd.read_csv('data/preprocessed-cs-students.csv')
+    train = data[data.termnum < 5]
+    test = data[data.termnum == 5]
+
+    # Predict new grades.
+    fm = FM(method='als')
+    fm.write(train, test)
+    predictions = fm.run()
+
+    # Evaluate predictions.
+    err = (test.grdpts.values - predictions)
+    abserr = abs(err)
+    mae = abserr.sum() / len(err)
+    mae_std = abserr.var()
+    sqerr = err ** 2
+    mse = sqerr.sum() / len(sqerr)
+    rmse = np.sqrt(mse)
+
+    print 'MAE:  %.4f' % mae
+    print 'STD:  %.4f' % mae_std
+    print 'RMSE: %.4f' % rmse
