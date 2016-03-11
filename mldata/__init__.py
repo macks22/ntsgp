8d0f6313e15c8a3be4c252921b3e9f615c3c2e79
Mack Sweeney <mackenzie.sweeney@gmail.com>
2016-03-11 12:27:45 -0500

    enable package-level import of key classes

---
+++ mldata/__init__.py
@@ -0,0 +1,20 @@
+from mldata import (
+    FeatureGuide, PandasDataset, PandasFullDataset, PandasTrainTestSplit)
+from model import (
+    Model, SklearnModel, ResultsBase, Results, RegressionResults, ResultsSet,
+    SklearnRegressionRunner, RegressionResultsSet)
+
+__all__ = [
+    'FeatureGuide',
+    'PandasDataset',
+    'PandasFullDataset',
+    'PandasTrainTestSplit',
+    'Model',
+    'SklearnModel',
+    'ResultsBase',
+    'Results',
+    'RegressionResults',
+    'ResultsSet',
+    'SklearnRegressionRunner',
+    'RegressionResultsSet'
+]
